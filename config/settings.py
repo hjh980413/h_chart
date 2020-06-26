57a820f15b96c95e48ea4a28bcddff839c315c50
hjh980413 <hjh980413@naver.com>
2020-06-26 23:55:22 +0900

    update

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9l9sw1l0-%qeuwv0-rvm#!rw%7o^h*6tu4zrreu(z^q!qfp4_m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
